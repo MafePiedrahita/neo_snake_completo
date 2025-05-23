f9b0e4f8d2a8e74591acbdd2b9c6474b594794bb
Mariana <mhenaoecheverri@gmail.com>
2025-05-23 12:18:44 -0500

    def controlar added

--- source/jugadores.py
+++ source/jugadores.py
@@ -7,3 +7,13 @@ class Jugador:
         self.teclas = teclas
         self.color = color
         self.puntaje = 0
+
+    def controlar(self, evento):
+        if evento.key == self.teclas.get("UP"):
+            self.serpiente.cambiar_direccion("ARRIBA")
+        elif evento.key == self.teclas.get("DOWN"):
+            self.serpiente.cambiar_direccion("ABAJO")
+        elif evento.key == self.teclas.get("LEFT"):
+            self.serpiente.cambiar_direccion("IZQUIERDA")
+        elif evento.key == self.teclas.get("RIGHT"):
+            self.serpiente.cambiar_direccion("DERECHA")
