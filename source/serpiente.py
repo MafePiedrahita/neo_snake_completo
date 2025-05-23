f3d56f6d1398405f97f2a815430c58bff71d0048
Mariana <mhenaoecheverri@gmail.com>
2025-05-23 09:40:48 -0500

    Class cabeza, def __init__, def mover, added

--- source/serpiente.py
+++ source/serpiente.py
@@ -1,1 +1,17 @@
-#Clases de la serpiente (Cabeza, Cuerpo, Cola)
+#Clases de la serpiente (Cabeza, Cuerpo, Cola) Mariana
+class Cabeza:
+    def __init__(self, x,y):
+        self.posicion = (x, y)
+        self.direccion = "DERECHA"
+
+    def mover(self):
+        x,y = self.posicion
+        if self.direccion == "ARRIBA":
+            y -= 1
+        elif self.direccion == "ABAJO":
+            y += 1
+        elif self.direccion == "IZQUIERDA":
+            x -= 1
+        elif self.direccion == "DERECHA":
+            x += 1
+        self.posicion = (x, y)
