c1f9d7c3c36c14a0e50a594dcd965d3b35567992
Mariana <mhenaoecheverri@gmail.com>
2025-05-23 12:07:56 -0500

    Imports y def cargar_puntajes added

--- source/puntajes.py
+++ source/puntajes.py
@@ -1,1 +1,11 @@
-#Registor de mejores puntajes
+#Registor de mejores puntajes Mariana
+import json
+import os
+
+ARCHIVO = "mejores_puntajes.json"
+
+def cargar_puntajes():
+    if not os.path.exists(ARCHIVO):
+        return []
+    with open(ARCHIVO, "r") as f:
+        return json.load(f)
