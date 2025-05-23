8c83f3cd02a135a0b6dcebce30eb5e5c20db49a8
Mariana <mhenaoecheverri@gmail.com>
2025-05-23 12:14:32 -0500

    Import, class jugador and def __init__ added

--- source/jugadores.py
+++ source/jugadores.py
@@ -1,1 +1,9 @@
-#Clase jugador para multijugador
+#Clase jugador para multijugador Mariana
+from serpiente import Serpiente
+
+class Jugador:
+    def __init__(self, teclas, color):
+        self.serpiente = Serpiente()
+        self.teclas = teclas
+        self.color = color
+        self.puntaje = 0
