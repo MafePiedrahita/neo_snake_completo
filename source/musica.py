4f05175ecd74dbaf05fa4b45936b1479a2e9e472
Mariana <mhenaoecheverri@gmail.com>
2025-05-24 13:44:10 -0500

    Corrección código archivo musica.py

--- source/musica.py
+++ source/musica.py
@@ -1,9 +1,12 @@
 #Reproducción de música con pygame.mixer
+from pathlib import Path
 import pygame.mixer
 
-def cargar_musica(ruta):
+def cargar_musica(ruta_relativa):
     pygame.mixer.init()
-    pygame.mixer.music.load(ruta)
+    base_dir = Path(__file__).resolve().parent
+    ruta_completa = (base_dir / ".." / ruta_relativa).resolve()
+    pygame.mixer.music.load(str(ruta_completa))
     pygame.mixer.music.set_volume(0.3)
     pygame.mixer.music.play(-1)
 
