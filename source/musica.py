514dc621e6d8c7d8beda9e86fb104e38329852e5
Mariana <mhenaoecheverri@gmail.com>
2025-05-24 09:55:38 -0500

    Import and def cargar_musica added

--- source/musica.py
+++ source/musica.py
@@ -1,1 +1,9 @@
 #Reproducción de música con pygame.mixer
+import pygame.mixer
+
+def cargar_musica(ruta):
+    pygame.mixer.init()
+    pygame.mixer.music.load(ruta)
+    pygame.mixer.music.set_volume(0.3)
+    pygame.mixer.music.play(-1)
+
