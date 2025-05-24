b1b045ccf0079b7a6991a38af8fba121560d4cb8
Mariana <mhenaoecheverri@gmail.com>
2025-05-24 18:04:00 -0500

    Imágenes agregadas a alimentos

--- source/alimentos.py
+++ source/alimentos.py
@@ -1,6 +1,6 @@
 import pygame
 import random
-TAM_CELDA = 30
+TAM_CELDA = 50
 
 verde = (0,128,0)
 morado = (128,0,128)
@@ -18,16 +18,22 @@ class Alimentos:
         self.y = random.randint(0, self.alto_tablero - 1)
 
     def dibujar(self, superficie):
-        rect = pygame.Rect(self.x * TAM_CELDA, self.y * TAM_CELDA, TAM_CELDA, TAM_CELDA)
-        pygame.draw.rect(superficie, self.color, rect)
+        pass
 
 class Pera(Alimentos):
     def __init__(self, ancho_tablero: int, alto_tablero: int):
-        super().__init__("Pera", ancho_tablero, alto_tablero, verde)
+        super().__init__("Pera", ancho_tablero, alto_tablero)
+        self.imagen = pygame.image.load("assets/imagenes/pera.png")
+        self.imagen = pygame.transform.scale(self.imagen, (TAM_CELDA, TAM_CELDA))
+
+    def dibujar(self, superficie):
+        superficie.blit(self.imagen, (self.x * TAM_CELDA, self.y * TAM_CELDA))
 
 class Ciruela(Alimentos):
     def __init__(self, ancho_tablero: int, alto_tablero: int):
-        super().__init__("Ciruela", ancho_tablero, alto_tablero, morado)
+        super().__init__("Ciruela", ancho_tablero, alto_tablero)
+        self.imagen = pygame.image.load("assets/imagenes/ciruela.png")
+        self.imagen = pygame.transform.scale(self.imagen, (TAM_CELDA, TAM_CELDA))
 
-
-
+    def dibujar(self, superficie):
+        superficie.blit(self.imagen, (self.x * TAM_CELDA, self.y * TAM_CELDA))
