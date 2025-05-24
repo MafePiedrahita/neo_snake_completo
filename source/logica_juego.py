e1735a1936e72eaead2e13cf79ef458c82e4f152
Mafe <mpiedrahita875@soyudemedellin.edu.co>
2025-05-24 12:09:16 -0500

    add def actualizar_estado

--- source/logica_juego.py
+++ source/logica_juego.py
@@ -9,3 +9,43 @@ def manejar_eventos(jugador1, jugador2):
             jugador1.controlar(evento)
             jugador2.controlar(evento)
     return True
+
+def actualizar_estado(jugadores, pera, ciruela, powerup, osbtaculos,
+                      duplicar_puntaje, tiempo_duplicar, tiempo_congelado,
+                      congelado, tiempo_actual, DURACION_POWERUP
+                      ):
+    for nombre, jugador in jugadores.items():
+        jugador.serpiente.mover()
+        cabeza = jugador.serpiente.cabeza.posicion
+
+        #comer pera
+        if cabeza == (pera.x, pera.y):
+            jugador.serpiente.crecer()
+            pera.nueva_posicion()
+            jugador.puntaje += 2 if duplicar_puntaje[nombre] else 1
+
+        #comer ciruela accion powerup
+        if cabeza == (ciruela.x, ciruela.y):
+            jugador.serpiente.crecer()
+            ciruela.nueva_posicion()
+            duplicar_puntaje[nombre] = True
+            tiempo_duplicar[nombre] = tiempo_actual
+
+        #tocar el congelar
+        if powerup.visible and cabeza == (powerup.x, powerup.y):
+            powerup.activar()
+            congelado = True
+            tiempo_congelado = tiempo_actual
+
+        #colsion obstaculo, cuerpo / borde
+        if jugador.serpiente.colisionar(osbtaculos=osbtaculos):
+            print(f"{nombre} colisionaste:(!!")
+            guardar_puntaje(nombre, jugador.puntaje)
+            return False, congelado, tiempo_congelado
+
+        #finaliza congelado
+        if congelado and tiempo_actual - tiempo_congelado > DURACION_POWERUP:
+            congelado = False
+
+        return True, congelado, tiempo_congelado
+
