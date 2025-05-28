176effb8005e81d747cc6be0a5920a7f5f32ccc2
Mafe <mpiedrahita875@soyudemedellin.edu.co>
2025-05-27 20:59:18 -0500

    correcion

--- source/powerups.py
+++ source/powerups.py
@@ -1,20 +1,20 @@
-import pygame
 import random
 
 TAM_CELDA = 50
-AZUL = (0, 0, 225)
 
 class PowerupCongelar:
-    def __init__(self, ancho_tablero, alto_tablero):
+    def __init__(self, ancho_tablero, alto_tablero, recursos):
         self.x = random.randint(0, ancho_tablero - 1)
         self.y = random.randint(0, alto_tablero - 1)
         self.visible = True
-        self.color = AZUL
+        self.imagen = recursos.get_imagen("powerup_congelar")
 
     def dibujar(self, superficie):
-        if self.visible:
-            rect = pygame.Rect(self.x * TAM_CELDA, self.y * TAM_CELDA, TAM_CELDA, TAM_CELDA)
-            pygame.draw.rect(superficie, self.color, rect)
+        if self.visible and self.imagen:
+            superficie.blit(self.imagen, (self.x * TAM_CELDA, self.y * TAM_CELDA))
 
     def activar(self):
         self.visible = False
+
+    def obtener_posicion(self):
+        return (self.x, self.y)
